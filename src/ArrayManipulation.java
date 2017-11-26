411480a8205ea9ff2e026f4808b5d55addf598a3
lovethesnipers@gmail.com <lovethesnipers@gmail.com>
2017-11-26 15:40:41 -0800

    Array_Manipulation

--- src/ArrayManipulation.java
+++ src/ArrayManipulation.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Created by surya on 11/25/17.
  */
@@ -6,19 +8,30 @@ public class ArrayManipulation {
     public static String output = "";
 
     public static void main(String [] args){
-        final int size = 5;
+
+        final  int size = 5;
         int [] array = new int [size];
 
         read_array_element(array, size);
         print_array_element(array, size);
+
+        JOptionPane.showMessageDialog(null, output);
     }
 
-    private static void print_array_element(int[] a, int row) {
-    }
-
-    private static void read_array_element(int[] a, int row) {
+    private static void print_array_element(int[] a, final int row) {
+        output = "\nThe array Elements are ";
         for(int i = 0; i<a.length; i++){
-            output += "The array elements are "+ row;
+            output+= " " + a[i];
+
+
         }
     }
+
+
+    private static void read_array_element(int[] a, final  int row) {
+        for(int i = 0; i<a.length; i++){
+            a[i] = Integer.parseInt(JOptionPane.showInputDialog("Enter " + row + " Numbers"));
+        }
+    }
+
 }
